e1d13e1b23ba7b426004acaccf7fc5a5a396caae
Ricardo Trevino <ricardotrevino24@gmail.com>
2021-10-12 12:07:19 -0500

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Ricardo Trevino on 10/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
