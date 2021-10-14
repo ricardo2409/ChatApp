b54599bef38578fb2ed2722f32520f7091220007
Ricardo Trevino <ricardotrevino24@gmail.com>
2021-10-14 10:13:24 -0500

    Adds log out button

--- ChatApp/Controllers/ConversationsViewController.swift
+++ ChatApp/Controllers/ConversationsViewController.swift
@@ -12,27 +12,21 @@ class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
         validateAuth()
-        
     }
 
     private func validateAuth() {
         
         if FirebaseAuth.Auth.auth().currentUser == nil {
-            
             let vc = LoginViewController()
             let nav = UINavigationController(rootViewController: vc)
             nav.modalPresentationStyle = .fullScreen
             present(nav, animated: false)
         }
-        
     }
 }
 
--- ChatApp/Resources/DatabaseManager.swift
+++ ChatApp/Resources/DatabaseManager.swift
@@ -21,7 +21,11 @@ final class DatabaseManager {
 extension DatabaseManager {
     
     public func userExists(with email: String, completion: @escaping ((Bool) -> Void)) {
-        database.child(email).observeSingleEvent(of: .value) { snapshot in
+        
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value) { snapshot in
             guard snapshot.value as? String != nil else {
                 completion(false)
                 return
@@ -32,7 +36,7 @@ extension DatabaseManager {
     
     /// Insert new user to databse
     public func insertUser(with user: ChatAppUser) {
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name": user.lastName
         ])
@@ -43,6 +47,12 @@ struct ChatAppUser {
     let firstName: String
     let lastName: String
     let emailAddress: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
     
     //  let profilePicUrl: String
     
